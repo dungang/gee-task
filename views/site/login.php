5d03a9d281d5e33e110e3b0faf20353447dc67fe
gang.dun <dungang@126.com>
2018-12-06 21:42:53 +0800

    调整readme

--- views/site/login.php
+++ views/site/login.php
@@ -3,20 +3,19 @@
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \app\forms\LoginForm */
-
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Login';
+$this->title = '登录';
 $this->params['breadcrumbs'][] = $this->title;
+$this->params = false;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
+	<h1 class="text-center">LESS IS MORE</h1>
+	<div class="row">
+		<div class="col-lg-4 col-lg-offset-4">
+			<h1><?= Html::encode($this->title) ?></h1>
+			<p>Please fill out the following fields to login:</p>
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
@@ -31,5 +30,5 @@ $this->params['breadcrumbs'][] = $this->title;
 
             <?php ActiveForm::end(); ?>
         </div>
-    </div>
+	</div>
 </div>
