4a35aa0f3a1bcf1ba466d76e9979fbb595adc3d6
gang.dun <dungang@126.com>
2018-12-11 14:46:18 +0800

    添加开发脚本

--- web/index-dev.php
+++ web/index-dev.php
@@ -4,13 +4,15 @@
 if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
     die('You are not allowed to access this file.');
 }
-
+// comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'test');
+defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+// require __DIR__ . '/../third/aliyun-log-sdk/Log_Autoload.php';
+// require __DIR__ . '/../third/aliyun-openapi-php-sdk/aliyun-php-sdk-core/Config.php';
 
-$config = require __DIR__ . '/../config/test.php';
+$config = require __DIR__ . '/../config/web.php';
 
 (new yii\web\Application($config))->run();
